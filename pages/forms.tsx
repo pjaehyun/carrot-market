62577ca12e486e5dc90fa4d71a8165c816893989
pjaehyun <show6756@gmail.com>
2022-11-17 23:48:39 +0900

    input validation with react-hook-form

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,29 +1,46 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
-// Less code (c)
 // Better validation
 // Better Errors (set, clear, display)
 // Have controll over inputs
-// Dont deal with events (c)
-// Easier inputs (c)
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log("im valid bb");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", { required: "Email is required" })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value="Create Account" />
     </form>
