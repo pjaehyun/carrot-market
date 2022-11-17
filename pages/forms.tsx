2abacd28c9a36b00e77af6500d07b75ede73612a
pjaehyun <show6756@gmail.com>
2022-11-17 23:27:44 +0900

    change input state with react-hook-form

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,61 +1,26 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// Less code (c)
+// Better validation
+// Better Errors (set, clear, display)
+// Have controll over inputs
+// Dont deal with events (c)
+// Easier inputs (c)
 
 export default function Forms() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [formErrors, setFormErrors] = useState("");
-  const [emailError, setEmailErrors] = useState("");
-
-  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-  };
-  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setEmailErrors("");
-    setEmail(value);
-  };
-  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setPassword(value);
-  };
-  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (username === "" || email === "" || password === "") {
-      setFormErrors("All firleds are required");
-    }
-    if (!email.includes("@")) {
-      setEmailErrors("Email is required");
-    }
-  };
+  const { register, watch } = useForm();
   return (
-    <form onSubmit={onSubmit}>
+    <form>
       <input
-        value={username}
-        onChange={onUsernameChange}
+        {...register("username")}
         type="text"
         placeholder="Username"
         required
-        minLength={5}
       />
+      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        value={email}
-        onChange={onEmailChange}
-        type="email"
-        placeholder="Email"
-        required
-      />
-      {emailError}
-      <input
-        value={password}
-        onChange={onPasswordChange}
+        {...register("password")}
         type="password"
         placeholder="Password"
         required
